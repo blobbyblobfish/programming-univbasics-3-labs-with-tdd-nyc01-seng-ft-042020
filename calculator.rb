9bd93d1c5eebc8d6a210e4513ccc5094fc100020
Blobby Blobfish <aywang6@gmail.com>
2020-03-17 21:31:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 # Add your variables here
-first_variable = 1.0;
-second_variable = 2.0;
-sum = first_variable + second_variable;
+first_number = 1.0;
+second_number = 2.0;
+sum = first_number + second_number;
